050277640918b6e82d694e3812c3fbb4d476dee4
ken <kenmugy@gmail.com>
2019-09-08 16:15:22 +0300

    feature(login-page):

--- UI/js/main.js
+++ UI/js/main.js
@@ -2,6 +2,9 @@ const signup_btn = document.querySelector('.btn-signup');
 const modal = document.querySelector('.modal-background');
 const cancel = document.querySelector('.cancel');
 const signup_page = document.querySelector('.sign-up-page');
+const login_btn = document.querySelector('.btn-login');
+const modal_login = document.querySelector('section.modal-background-login');
+const login_cancel = document.querySelector('.login-cancel');
 
 signup_btn.addEventListener('click', () => {
   modal.style.display = 'block';
@@ -9,4 +12,12 @@ signup_btn.addEventListener('click', () => {
 
 cancel.addEventListener('click', () => {
   modal.style.display = 'none';
+});
+
+login_btn.addEventListener('click', () => {
+  modal_login.style.display = 'block';
+});
+
+login_cancel.addEventListener('click', () => {
+  modal_login.style.display = 'none';
 });
